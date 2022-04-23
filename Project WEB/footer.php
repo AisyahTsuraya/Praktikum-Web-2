b3f51e4479df83e2ddcd6bbeac38240e93578883
AisyahTsuraya <92940160+AisyahTsuraya@users.noreply.github.com>
2022-04-24 04:34:20 +0700

    Add files via upload

---
+++ Project WEB/footer.php
@@ -0,0 +1,27 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <strong>Aisyah &copy; 2022 <a href="https://www.instagram.com/invites/contact/?i=jaftwmbaosxi&utm_content=2v3m2xt">Instagram</a>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+</body>
+</html>
---
+++ Project WEB/index.php
@@ -0,0 +1,70 @@
+<?php
+    include_once 'header.php';
+    include_once 'sidebar.php'
+?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <style>   
+        </style>
+        <meta charset="UTF-8" />
+        <title>Curriculum Vitae</title>
+    </head>
+    <body style="margin: 0; background-color: #fff; letter-spacing: 0.0083em;">
+        <div style="width: 100%; max-width: none;">
+            <div style="display: flex; justify-content: space-between; align-items: flex-end;">
+                <h1></h1>
+                <ceenter><h2><u>Aisyah Tsuraya Rafilah</u></h2></ceenter>
+                    <br>
+            </div>
+        </div>
+        <section>
+        <h2><u>Biodata</u></h2>
+        <div>
+                <p><b>NAMA : Aisyah Tsuraya Rafilah</p></b>
+                <p><b>Tempat, Tanggal Lahir : Depok, 15 agustus 2003</p></b>
+                <p><b>Jenis Kelamin : Perempuan</p></b>
+                <p><b>Agama : Islam</p></b>
+                <p><b>Pekerjaan : Mahasiswa</p></b>
+                <p><b>Alamat : Jalan delima ujung l no 101 tanah baru beji , depok</p></b>
+                
+        </div>
+    </section>
+
+    <section>
+        <h2><u>Pendidikan Terakhir</u></h2>
+        <div>
+            <ul>
+                <li>
+                <li>SDN 6 Mangunreja</li>
+                <li>SMPN 3 Depok</li> 
+                <li>SMAN 5 Depok</li>
+                <li> Sistem Informasi STT NF</li>
+            </ul>
+        </div>
+    </section>
+
+    <section>
+        <h2><u>Hobi</u></h2>
+        <div>
+            <p>Marketing, Travelling,Video games</p>
+        </div>
+    </section>
+    <footer>
+        <h2><u>Contact Me</u></h2>
+        <div>
+        <p><b>Instagram : Tsuraya_02</p></b>
+        <p><b>Whatshapp : 081398719899</p></b>
+        </div>
+    </footer>
+    </body>
+</html>
+    <!-- /.content -->
+  </div>
+
+
+
+    include_once'footer.php'
+?>
